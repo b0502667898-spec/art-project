08492bfeb824b1afa5e424756a0636e888134fa5
b0502667898-spec <b0502667898@gmail.com>
2026-03-10 11:30:02 +0200

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import tensorflow as tf
-tf.keras.backend.clear_session()
+from tensorflow.keras.models import load_model
+import tensorflow.keras.backend as K
+# ניקוי זיכרון לפני הטעינה
+K.clear_session()
 from PIL import Image
 import numpy as np
 import os
@@ -27,7 +30,7 @@ DRIVE_URL = "https://drive.google.com/file/d/1kIZPNmXPCGHn4IXB-nxSubwpnwnvyr2e/v
 
 # הורדה וטעינה
 download_file_from_google_drive(DRIVE_URL, MODEL_PATH)
-model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+model = load_model(MODEL_PATH, compile=False, safe_mode=False)
 
 # רשימת האמנים (לפי הסדר שהמודל אומן)
 ARTISTS = ["Goya", "Monet", "Raphael", "Vincent_van_Gogh", "William_Blake"]
@@ -62,4 +65,5 @@ if uploaded_file is not None:
 
     st.success(f"המודל מזהה שזהו ציור של: **{HEB_NAMES[predicted_artist]}**")
     st.info(f"רמת ביטחון: {confidence:.2f}%")
+
 
